ad4088583c9d5e87c9ddba69f30a98e9a3d0391f
hana <hdrasko@gmail.com>
2022-01-28 05:34:49 +0100

    update production 1

--- cheekycv_new_1/settings.py
+++ cheekycv_new_1/settings.py
@@ -5,8 +5,7 @@ import django_heroku
 import dj_database_url
 from decouple import config
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +83,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+# DATABASES = { 'default': dj_database_url.config() }
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
