8e8c3cfc7c1197746be72fec64ecaeecf1292397
hana <hdrasko@gmail.com>
2022-07-25 23:51:51 +0800

    1

--- cheekycv_new_1/settings.py
+++ cheekycv_new_1/settings.py
@@ -149,13 +149,13 @@ LOGOUT_REDIRECT_URL = 'home'
 
 #SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'CHEEKY CV Team <noreply@cheekycv.com>'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
 
