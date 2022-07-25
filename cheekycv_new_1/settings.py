748aa44db5db9690c9ff698f069dc4c5673545ff
hana <hdrasko@gmail.com>
2022-07-26 01:14:03 +0800

    eelo

--- cheekycv_new_1/settings.py
+++ cheekycv_new_1/settings.py
@@ -18,8 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
