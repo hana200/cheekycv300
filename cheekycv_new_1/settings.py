a45be3780d5cf02af1b04816bcef4eac6149aff3
hana <hdrasko@gmail.com>
2022-07-27 20:57:10 +0800

    vars

--- cheekycv_new_1/settings.py
+++ cheekycv_new_1/settings.py
@@ -9,66 +9,8 @@ import dj_database_url
 from decouple import config
 from dotenv import load_dotenv
 
-import logging
-import logging.handlers
-logger = logging.getLogger("my_logger")
-logger.setLevel(logging.DEBUG)
-handler = logging.handlers.SysLogHandler(
-    facility=logging.handlers.SysLogHandler.LOG_DAEMON, address="/dev/log")
-
 DJANGO_LOG_LEVEL= True
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
-    },
-    'filters': {
-        'special': {
-            '()': 'project.logging.SpecialFilter',
-            'foo': 'bar',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['special']
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'cheekycv_new_1.custom': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'INFO',
-            'filters': ['special']
-        }
-    }
-}
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
